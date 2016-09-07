24b1977cbb82b503a7624ef25e3ae900ee7467ce
b1u3c1ph3r <shuler.david.p@gmail.com>
2016-09-07 07:25:43 -0500

    Added changes to run in Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: Python flask_test_one.py
--- flask_test_one.py
+++ flask_test_one.py
@@ -19,7 +19,6 @@ def index():
 def about():
     return render_template('about.html')
 
-
-
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000)
+    app.run(host="0.0.0.0", port=port)
